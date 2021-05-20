ab0a09624186cb8fe765aadbe9cdd04fea8eef31
liebmeister <philippe.liebens@skynet.be>
2021-05-20 09:41:26 +0200

    Initial commit

---
+++ android/app/src/main/java/eu/philibi/prono/MainActivity.java
@@ -0,0 +1,6 @@
+package eu.philibi.prono;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/eu/philibi/prono/MainActivity.kt
@@ -0,0 +1,6 @@
+package eu.philibi.prono
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
